5717527478f0352d64aa35bc4453859f878eb363
elgoog <elgoog.elibom@gmail.com>
2014-05-31 15:02:59 +0900

    refactoring during a training class.

--- name_index.rb
+++ name_index.rb
@@ -2,20 +2,19 @@
 class NameIndex
   def self.index_category_of(character)
     categories = ['ア', 'カ', 'サ', 'タ', 'ナ',  'ハ', 'マ', 'ヤ', 'ラ', 'ワ', 'ン']
-    categories_code = categories.map { |x| x.ord }
+    categories_code = categories.map(&:ord)
     char_code = character.ord
 
-    loop do
-      break if categories_code.include?(char_code)
+    until categories_code.include?(char_code)
       char_code -= 1
     end
+
     char_code.chr('UTF-8')
   end
 
   def self.create_index(names)
-    indexed_name_hash = names.group_by { |val| self.index_category_of(val[0]) }
-    indexed_name_hash.each_value { |name_array| name_array.sort! }
-    indexed_name_hash.to_a.sort_by { |x| x[0] }
+    indexed_name_hash = names.sort.group_by { |val| self.index_category_of(val[0]) }
+    indexed_name_hash.to_a.sort
   end
 end
 
