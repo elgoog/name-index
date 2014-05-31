45c52b871ab25634c98126ec5ccd4447d667dfaa
elgoog <elgoog.elibom@gmail.com>
2014-05-31 23:48:15 +0900

    remove a bug.

--- name_index.rb
+++ name_index.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 class NameIndex
-  CATEGORIES_CODE = %w(ア, カ, サ, タ, ナ,  ハ, マ, ヤ, ラ, ワ, ン).map(&:ord).freeze
+  CATEGORIES_CODE = %w(ア カ サ タ ナ ハ マ ヤ ラ ワ ン).map(&:ord).freeze
   def self.index_category_of(initial_character)
     char_code = initial_character.ord
     char_code -= 1 until CATEGORIES_CODE.include?(char_code)
